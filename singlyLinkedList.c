8674857c324028044e7fd2f6da4d934dd6a9a282
Twinkle Chawla <twinkle1chawla@gmail.com>
2016-10-07 00:05:20 +0530

    initialized a singlyLL.c

---
+++ singlyLinkedList.c
@@ -0,0 +1,10 @@
+#
+struct Node{
+	int data;
+	sturct Node *next;
+}
+
+void insertNode(struct node **head, int data, int position){
+	struct Node *newNode;
+	newNode = (struct Node *)malloc(sizeof(struct Node));
+}
